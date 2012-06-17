3c986211fd2b084022783ccb20c300263deec434
maxdeliso <maxdeliso@gmail.com>
2012-06-17 10:37:36 -0700

    added event calls

--- srv/yarg.js
+++ srv/yarg.js
@@ -3,13 +3,23 @@ var io = require('socket.io').listen(80);
 io.sockets.on('connection', function(socket) {
     io.sockets.emit('new', { msg: 'new client connected'});
 
-    socket.on('message', function(from, msg) {
-        console.log(
-            'server received message from ', from, 
-            ' with text ', msg);
+    socket.on('message', function(msg) {
+        console.log('server received message', msg);
     });
     
     socket.on('disconnect', function() {
         console.log('client disconnected');
+    });
+    
+    socket.on('click', function(msg) {
+        console.log('click: ', msg);
+    });
+    
+    socket.on('keydown', function(msg) {
+       console.log('keydown: ', msg);
+    });
+    
+    socket.on('keyup', function(msg) {
+       console.log('keyup: ', msg);
     });
 });
